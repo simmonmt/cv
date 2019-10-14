8e6e983ee756afe039fbb3b3de90cee043f7beb5
Matthew Simmons <simmonmt@acm.org>
2019-10-14 17:56:11 -0400

    factor out image reading and thresholding

--- qrcode/qr_main.cc
+++ qrcode/qr_main.cc
@@ -10,25 +10,36 @@ static bool IsNonEmptyMessage(const char* flagname, const std::string& value) {
 }
 DEFINE_validator(input, &IsNonEmptyMessage);
 
-int main(int argc, char** argv) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+namespace {
 
-  constexpr char kWindowName[] = "Output";
-
-  cv::Mat input = cv::imread(FLAGS_input, cv::IMREAD_COLOR);
+int readBwImage(const std::string& path, cv::OutputArray out) {
+  cv::Mat input = cv::imread(path, cv::IMREAD_COLOR);
   if (!input.data) {
-    std::cerr << "failed to read image\n";
     return -1;
   }
 
   cv::Mat gray;
   cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
+  cv::threshold(gray, out, 127, 255, cv::THRESH_BINARY);
 
-  cv::Mat bw;
-  cv::threshold(gray, bw, 127, 255, cv::THRESH_BINARY);
+  return 0;
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  constexpr char kWindowName[] = "Output";
+
+  cv::Mat image;
+  if (readBwImage(FLAGS_input, image) < 0) {
+    std::cerr << "failed to read image\n";
+    return -1;
+  }
 
   cv::namedWindow(kWindowName, 1);
-  cv::imshow(kWindowName, bw);
+  cv::imshow(kWindowName, image);
   cv::waitKey(0);
 
   return 0;
