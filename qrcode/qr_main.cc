685136a465b038585bfad0b59ec4d0a1e65d750d
Matthew Simmons <simmonmt@acm.org>
2019-10-14 18:22:05 -0400

    walk pixels

--- qrcode/qr_main.cc
+++ qrcode/qr_main.cc
@@ -1,3 +1,4 @@
+#include "absl/strings/str_format.h"
 #include "gflags/gflags.h"
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgcodecs.hpp"
@@ -30,17 +31,34 @@ int readBwImage(const std::string& path, cv::OutputArray out) {
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  constexpr char kWindowName[] = "Output";
-
   cv::Mat image;
   if (readBwImage(FLAGS_input, image) < 0) {
     std::cerr << "failed to read image\n";
     return -1;
   }
 
-  cv::namedWindow(kWindowName, 1);
-  cv::imshow(kWindowName, image);
-  cv::waitKey(0);
+  if (image.depth() != CV_8U || image.channels() != 1 ||
+      !image.isContinuous()) {
+    std::cerr << absl::StrFormat("expected depth %d, got %d, chans 1, got %d, "
+				 "continuous, got %d\n",
+				 CV_8U, image.depth(), image.channels(),
+				 image.isContinuous());
+    return -1;
+  }
+
+  uchar* p = image.ptr<uchar>(0);
+  for (int row = 0; row < image.rows; ++row) {
+    int num = 0;
+    for (int col = 0; col < image.cols; ++col) {
+      num += p[row*image.cols + col] == 0;
+    }
+    std::cout << absl::StrFormat("row %d: %d\n", row, num);
+  }
+
+  // constexpr char kWindowName[] = "Output";
+  // cv::namedWindow(kWindowName, 1);
+  // cv::imshow(kWindowName, image);
+  // cv::waitKey(0);
 
   return 0;
 }
