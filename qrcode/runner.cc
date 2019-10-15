aa9864fcd0facdf678e0d884b4b2d59792cd69ff
Matthew Simmons <simmonmt@acm.org>
2019-10-14 20:32:26 -0400

    add unoptimized version

--- qrcode/runner.cc
+++ qrcode/runner.cc
@@ -1,1 +1,39 @@
 #include "qrcode/runner.h"
+
+namespace {
+
+int run_len(absl::Span<const unsigned char> vals) {
+  const unsigned char want = vals.front();
+  int len = 1;
+  for (int i = 1; i < vals.size() && vals[i] == want; i++) {
+    ++len;
+  }
+  return len;
+}
+
+}  // namespace
+
+Runner::Runner(absl::Span<const unsigned char> vals)
+  : vals_(vals),
+    start_(0) {}
+
+absl::optional<std::vector<int>> Runner::Next(const int num, int* idx) {
+  std::vector<int> lens(num);
+
+  absl::Span<const unsigned char> left = vals_;
+  for (int i = 0; i < lens.size(); ++i) {
+    if (left.empty()) {
+      return absl::nullopt;
+    }
+    int len = run_len(left);
+    lens[i] = len;
+    left = left.subspan(len);
+  }
+
+  *idx = start_;
+
+  start_ += lens[0];
+  vals_ = vals_.subspan(lens[0]);
+
+  return lens;
+}
--- qrcode/runner.h
+++ qrcode/runner.h
@@ -1,21 +1,23 @@
 #ifndef _QRCODE_RUNNER_H_
 #define _QRCODE_RUNNER_H_ 1
 
+#include <vector>
+
 #include "absl/types/optional.h"
 #include "absl/types/span.h"
 
 class Runner {
  public:
-  Runner(absl::Span<unsigned char> vals)
-    : vals_(vals) {}
+  Runner(absl::Span<const unsigned char> vals);
   ~Runner() = default;
 
   Runner(const Runner&) = delete;
 
-  absl::optional<std::vector<int>> Next();
+  absl::optional<std::vector<int>> Next(const int num, int* idx);
 
  private:
-  absl::Span<unsigned char> vals_;
+  absl::Span<const unsigned char> vals_;
+  int start_;
 };
 
 #endif // _QRCODE_RUNNER_H_
