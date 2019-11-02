896b70cc184b13bd83dc2e8c6df2d72ecb2fb726
Matthew Simmons <simmonmt@acm.org>
2019-11-02 18:59:45 -0400

    add a locate test for a non-tilted code

--- qrcode/qr_locate_test.cc
+++ qrcode/qr_locate_test.cc
@@ -10,26 +10,55 @@ using ::testing::DoubleNear;
 using ::testing::Eq;
 using ::testing::VariantWith;
 
-constexpr char kTestImageRelPath[] = "qrcode/testdata/tilt.png";
+constexpr char kTiltImageRelPath[] = "qrcode/testdata/tilt.png";
+constexpr char kStraightImageRelPath[] = "qrcode/testdata/straight.png";
 
-TEST(LocateCodeTest, Test) {
-  cv::Mat image = cv::imread(kTestImageRelPath, cv::IMREAD_GRAYSCALE);
-  ASSERT_TRUE(image.data != nullptr);
+class LocateCodeTest : public ::testing::Test {
+ public:
+  void TestImage(const std::string& path, const PositioningPoints& points,
+                 const Point& center, double rotation_angle) {
+    cv::Mat image = cv::imread(path, cv::IMREAD_GRAYSCALE);
+    ASSERT_TRUE(image.data != nullptr) << path;
 
-  auto result = LocateCode(image);
-  ASSERT_THAT(result, VariantWith<std::unique_ptr<LocatedCode>>(_));
-  std::unique_ptr<LocatedCode> located_code =
-      std::move(absl::get<std::unique_ptr<LocatedCode>>(result));
+    auto result = LocateCode(image);
+    ASSERT_THAT(result, VariantWith<std::unique_ptr<LocatedCode>>(_))
+        << "had string: " << absl::get<std::string>(result);
+    std::unique_ptr<LocatedCode> located_code =
+        std::move(absl::get<std::unique_ptr<LocatedCode>>(result));
 
+    EXPECT_THAT(located_code->positioning_points, Eq(points));
+    EXPECT_THAT(located_code->center, Eq(center));
+    EXPECT_THAT(located_code->rotation_angle,
+                DoubleNear(rotation_angle, 0.025));
+  }
+};
+
+TEST_F(LocateCodeTest, Straight) {
+  PositioningPoints expected_points = {
+      {669, 684},
+      {1525, 677},
+      {672, 1542},
+  };
+
+  Point expected_center(1099, 1110);
+  double expected_angle = 0.2;
+
+  TestImage(kStraightImageRelPath, expected_points, expected_center,
+            expected_angle);
+}
+
+TEST_F(LocateCodeTest, Tilt) {
   PositioningPoints expected_points = {
       {1016, 514},
       {1705, 1014},
       {506, 1203},
   };
 
-  EXPECT_THAT(located_code->positioning_points, Eq(expected_points));
-  EXPECT_THAT(located_code->center, Eq(Point(1105, 1109)));
-  EXPECT_THAT(located_code->rotation_angle, DoubleNear(-36.5, 0.25));
+  Point expected_center(1105, 1109);
+  double expected_angle = -36.5;
+
+  TestImage(kTiltImageRelPath, expected_points, expected_center,
+            expected_angle);
 }
 
 }  // namespace
