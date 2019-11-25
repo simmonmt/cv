db013c8cd5a3ec467132cee6c45d17c5c7490c47
Matthew Simmons <simmonmt@acm.org>
2019-11-24 19:09:02 -0500

    decoder works for 15,5, not yet for 18,6

--- qrcode/bch_test.cc
+++ qrcode/bch_test.cc
@@ -10,18 +10,30 @@ namespace {
 using ::testing::ElementsAreArray;
 using ::testing::VariantWith;
 
+// For whatever reason VariantWith doesn't print the string (i.e. the other
+// value), so this wrapper does it.
+absl::variant<std::vector<bool>, std::string> CallDecodeBCH(
+    const GF& gf, const std::vector<bool>& bits) {
+  auto out = DecodeBCH(gf, bits);
+  if (absl::holds_alternative<std::string>(out)) {
+    std::cout << "decoder returned error: " << absl::get<std::string>(out)
+              << "\n";
+  }
+  return out;
+}
+
 TEST(BCHDecoderTest, Test15_5) {
   const std::vector<bool> ref = {0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1};
 
   // No errors
-  EXPECT_THAT(DecodeBCH(GF16(), ref),
+  EXPECT_THAT(CallDecodeBCH(GF16(), ref),
               VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
 
   // Two errors
   std::vector<bool> input = ref;
   input[5] = !input[5];
   input[13] = !input[13];
-  EXPECT_THAT(DecodeBCH(GF16(), input),
+  EXPECT_THAT(CallDecodeBCH(GF16(), input),
               VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
 }
 
@@ -29,14 +41,14 @@ TEST(BCHDecoderTest, Test18_6) {
   const std::vector<bool> ref = {0, 0, 1, 0, 1, 0, 0, 1, 0,
                                  0, 1, 1, 1, 1, 1, 0, 0, 0};
 
-  EXPECT_THAT(DecodeBCH(GF32(), ref),
+  EXPECT_THAT(CallDecodeBCH(GF32(), ref),
               VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
 
   // Two errors
   std::vector<bool> input = ref;
   input[5] = !input[5];
   input[13] = !input[13];
-  EXPECT_THAT(DecodeBCH(GF32(), input),
+  EXPECT_THAT(CallDecodeBCH(GF32(), input),
               VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
 }
 
