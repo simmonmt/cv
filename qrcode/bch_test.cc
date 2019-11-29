242a870d8d687a5217bc367e76feedc76154f2e9
Matthew Simmons <simmonmt@acm.org>
2019-11-29 14:43:04 -0500

    remove 18_6 test since it doesn't work
    also there's no such thing as BCH(18,6)

--- qrcode/bch_test.cc
+++ qrcode/bch_test.cc
@@ -48,22 +48,4 @@ TEST(BCHDecoderTest, Test15_5_QRStandard) {
               VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
 }
 
-TEST(BCHDecoderTest, Test18_6) {
-  // const std::vector<bool> ref = {0, 0, 1, 0, 1, 0, 0, 1, 0,
-  //                                0, 1, 1, 1, 1, 1, 0, 0, 0};
-  const std::vector<bool> ref = {0, 0, 0, 1, 1, 1, 1, 1, 0,
-                                 0, 1, 0, 0, 1, 0, 1, 0, 0};
-
-  EXPECT_THAT(CallDecodeBCH(GF32(), ref),
-              VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
-  return;
-
-  // Two errors
-  std::vector<bool> input = ref;
-  input[5] = !input[5];
-  input[13] = !input[13];
-  EXPECT_THAT(CallDecodeBCH(GF32(), input),
-              VariantWith<std::vector<bool>>(ElementsAreArray(ref)));
-}
-
 }  // namespace
