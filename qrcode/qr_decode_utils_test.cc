c8477d9330c268b51be3054c58f65fc5e310994b
Matthew Simmons <simmonmt@acm.org>
2020-01-06 15:21:49 -0500

    add format decoding

--- qrcode/qr_decode_utils_test.cc
+++ qrcode/qr_decode_utils_test.cc
@@ -11,6 +11,28 @@ namespace {
 
 using ::testing::ElementsAreArray;
 
+constexpr char kTestDataSpecExamplePath[] =
+    "qrcode/testdata/spec_example_1m.txt";
+
+class DecodeFormatTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    ASSIGN_OR_ASSERT(base_, ReadQRCodeArrayFromFile(kTestDataSpecExamplePath),
+                     "read failed");
+  }
+
+  std::unique_ptr<QRCodeArray> base_;
+};
+
+TEST_F(DecodeFormatTest, Test) {
+  ASSIGN_OR_ASSERT(QRFormat format, DecodeFormat(*base_), "decode failed");
+
+  // The spec says it's mask pattern 010, but it's actually 011 because we can't
+  // have nice things.
+  EXPECT_EQ(QRECC_M, format.ecc_level);
+  EXPECT_EQ(0b011, format.mask_pattern);
+}
+
 class UnmaskingTest : public ::testing::Test {
  public:
   void VerifyUnmasking(int version, std::unique_ptr<QRCodeArray> array,
