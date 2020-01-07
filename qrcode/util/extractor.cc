4fea9ef0ec163581dbe9696e7e442ae2979b1c3b
Matthew Simmons <simmonmt@acm.org>
2020-01-07 12:28:07 -0500

    dump qrformat, timing information

--- qrcode/util/extractor.cc
+++ qrcode/util/extractor.cc
@@ -1,18 +1,30 @@
+#include <bitset>
 #include <iostream>
 #include <memory>
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+#include "absl/time/clock.h"
 #include "absl/types/variant.h"
 #include "opencv2/opencv.hpp"
 
 #include "qrcode/cv_utils.h"
 #include "qrcode/point.h"
+#include "qrcode/qr_error_characteristics_types.h"
 #include "qrcode/qr_extract.h"
+#include "qrcode/qr_format.h"
 #include "qrcode/qr_locate.h"
 #include "qrcode/qr_normalize.h"
 
 ABSL_FLAG(std::string, input, "", "Input file");
+
+struct PointInTime {
+  PointInTime(const std::string& name, const absl::Time& time)
+      : name(name), time(time) {}
+
+  std::string name;
+  absl::Time time;
+};
 
 int main(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
@@ -22,11 +34,15 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  std::vector<PointInTime> times = {{"start", absl::Now()}};
+
   cv::Mat image;
   if (!ReadBwImage(absl::GetFlag(FLAGS_input), image)) {
     std::cerr << "failed to read image\n";
     return -1;
   }
+
+  times.emplace_back("read", absl::Now());
 
   auto maybe_located_code = LocateCode(image);
   if (absl::holds_alternative<std::string>(maybe_located_code)) {
@@ -37,6 +53,8 @@ int main(int argc, char** argv) {
   std::unique_ptr<LocatedCode> located_code =
       std::move(absl::get<std::unique_ptr<LocatedCode>>(maybe_located_code));
 
+  times.emplace_back("locate", absl::Now());
+
   auto maybe_qr_image = NormalizeCode(image, *located_code);
   if (absl::holds_alternative<std::string>(maybe_qr_image)) {
     std::cerr << "failed to normalize code: "
@@ -45,6 +63,8 @@ int main(int argc, char** argv) {
   }
   std::unique_ptr<QRImage> qr_image =
       std::move(absl::get<std::unique_ptr<QRImage>>(maybe_qr_image));
+
+  times.emplace_back("normalize", absl::Now());
 
   auto maybe_array = ExtractCode(*qr_image);
   if (absl::holds_alternative<std::string>(maybe_array)) {
@@ -55,11 +75,35 @@ int main(int argc, char** argv) {
   std::unique_ptr<QRCodeArray> array =
       std::move(absl::get<std::unique_ptr<QRCodeArray>>(maybe_array));
 
+  times.emplace_back("extract", absl::Now());
+
   for (int y = 0; y < array->height(); ++y) {
     for (int x = 0; x < array->width(); ++x) {
       std::cout << (array->Get(Point(x, y)) ? "X" : " ");
     }
     std::cout << "\n";
+  }
+
+  std::cout << "\n";
+
+  auto maybe_format = DecodeFormat(*array);
+  if (absl::holds_alternative<std::string>(maybe_format)) {
+    std::cerr << "failed to decode format: "
+              << absl::get<std::string>(maybe_format) << "\n";
+    return -1;
+  }
+  const QRFormat format = std::move(absl::get<QRFormat>(maybe_format));
+
+  std::cout << "ECC " << format.ecc_level << " mask "
+            << std::bitset<3>(format.mask_pattern) << "\n";
+
+  std::cout << "Timing:\n";
+
+  for (int i = 1; i < times.size(); ++i) {
+    const PointInTime& ref = times[i - 1];
+    const PointInTime& cur = times[i];
+
+    std::cout << "  " << cur.name << ": " << cur.time - ref.time << "\n";
   }
 
   return 0;
